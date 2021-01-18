16bf8bb8430a7aed56c6ac5ab0fa6cb381a8c1a8
saumikkhan <saumikkhan@crescentschool.org>
2021-01-17 23:59:29 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+app.use("/public",express.static("public"));
+
+
+
+app.get("/", function (req,res) {
+
+    res.sendFile(__dirname +"/public/index.html");
+
+    });
+
+
+
+
+app.listen("3000");
